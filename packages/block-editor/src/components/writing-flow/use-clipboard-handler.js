6aab221f3e0b6645a5c7d1a173bc835800d4d19c
ella <ella@automattic.com>
2026-07-29 18:59:19 -0700

    Revert "Show a snackbar when the pasted blocks cannot be inserted"
    This reverts commit 4ee6c344518a12a0cbc04fc485441b9bd3ed6074.

--- packages/block-editor/src/components/writing-flow/use-clipboard-handler.js
+++ packages/block-editor/src/components/writing-flow/use-clipboard-handler.js
@@ -14,8 +14,6 @@ import {
 } from '@wordpress/dom';
 import { useDispatch, useRegistry, useSelect } from '@wordpress/data';
 import { useRefEffect } from '@wordpress/compose';
-import { __ } from '@wordpress/i18n';
-import { store as noticesStore } from '@wordpress/notices';
 
 /**
  * Internal dependencies
@@ -238,15 +236,6 @@ export default function useClipboardHandler() {
 								: [ block ];
 
 						if ( ! switchedBlocks ) {
-							registry
-								.dispatch( noticesStore )
-								.createWarningNotice(
-									__(
-										'The copied blocks cannot be pasted here.'
-									),
-									{ type: 'snackbar' }
-								);
-							event.preventDefault();
 							return;
 						}
 
